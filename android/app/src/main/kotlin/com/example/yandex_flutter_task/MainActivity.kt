c015621f0581c4f18fc6ff0e345b1728bc7f614b
Evgeny Alekseenko <punconge@gmail.com>
2023-06-10 21:50:43 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/yandex_flutter_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yandex_flutter_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:yandex_flutter_task/app.dart';
+
+void main() {
+  runApp(const App());
+}
