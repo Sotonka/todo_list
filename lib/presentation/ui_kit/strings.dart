27536d9a72f254e3f9df3322ebec652e4f4a0648
Evgeny Alekseenko <punconge@gmail.com>
2023-06-11 13:01:56 +1000

    strings

---
+++ lib/presentation/ui_kit/strings.dart
@@ -0,0 +1,18 @@
+class AppStrings {
+  static const String cafe = 'кафе';
+  static const String mainTitleTodos = 'Мои дела';
+  static const String mainTitleDone = 'Выполнено';
+  static const String mainListNew = 'Новое';
+  static const String todoSave = 'Сохранить';
+  static const String todoDelete = 'Удалить';
+  static const String todoCalendarCancel = 'Отмена';
+  static const String todoCalendarDone = 'Готово';
+  static const String todoFormHint = 'Что надо сделать...';
+  static const String todoFormImportance = 'Важность';
+  static const String todoFormDeadline = 'Сделать до';
+  static const String todoImportanceNo = 'Нет';
+  static const String todoImportanceLow = 'Низкий';
+  static const String todoImportanceHigh = '!! Высокий';
+
+  AppStrings._();
+}
---
+++ lib/presentation/ui_kit/ui_kit.dart
@@ -0,0 +1,5 @@
+export 'colors.dart';
+export 'icons.dart';
+export 'strings.dart';
+export 'text_styles.dart';
+export 'themes.dart';
