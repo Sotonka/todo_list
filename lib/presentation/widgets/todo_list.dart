8cea7f23ecba4b659124bdc9cbd13425b3c0729d
Evgeny Alekseenko <punconge@gmail.com>
2023-06-14 12:57:58 +1000

    fix

--- lib/presentation/widgets/todo_list.dart
+++ lib/presentation/widgets/todo_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yandex_flutter_task/domain/model/todo.dart';
 import 'package:yandex_flutter_task/presentation/providers/visibility_provider.dart';
+import 'package:yandex_flutter_task/presentation/ui_kit/ui_kit.dart';
 import 'package:yandex_flutter_task/presentation/widgets/todo_tile.dart';
 
 class TodoList extends ConsumerWidget {
@@ -15,6 +16,8 @@ class TodoList extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final visibilityProvider = ref.watch(visibilityStateNotifierProvider);
+    final theme = Theme.of(context);
+    final themeColors = theme.extension<AppThemeColors>()!;
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(8),
@@ -25,6 +28,17 @@ class TodoList extends ConsumerWidget {
               TodoTile(
                 todo: todo,
               ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 14, bottom: 14, left: 52),
+              child: Text(
+                AppStrings.mainListNew,
+                style: theme.primaryTextTheme.bodyMedium!
+                    .copyWith(color: themeColors.labelTetriary),
+              ),
+            ),
+          ),
         ],
       ),
     );
